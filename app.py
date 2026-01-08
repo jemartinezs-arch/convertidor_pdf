a34e8c9d4037a90157ad2a5f3fab2e99f290e547
MACBOOK PRO <macbookpro@MBP-de-MACBOOK.lan>
2026-01-07 19:08:23 -0500

    restaurar pdf backend

--- app.py
+++ app.py
@@ -239,8 +239,8 @@ def pdf_to_word():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # usa PORT si existe, si no usa 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
